c1a270c480882f615568b38a2deb84ea6a57ca0c
Jiri Raska <jiri@iraska.cz>
2026-09-09 22:30:05 +0200

    test(delegation): pin approval policy event contract
    Signed-off-by: Jiri Raska <jiri@iraska.cz>

--- openbank-delegation-service/src/test/kotlin/com/openbank/delegation/contract/DelegationEventPactFolderProviderVerificationTest.kt
+++ openbank-delegation-service/src/test/kotlin/com/openbank/delegation/contract/DelegationEventPactFolderProviderVerificationTest.kt
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.openbank.delegation.domain.event.DelegationActivated
 import com.openbank.delegation.domain.event.DelegationRevoked
 import com.openbank.delegation.domain.event.EventMoney
+import com.openbank.delegation.domain.model.ApprovalPolicy
 import com.openbank.delegation.domain.model.DelegationCapability
 import com.openbank.delegation.domain.model.DelegationResourceType
 import org.junit.jupiter.api.BeforeEach
@@ -118,6 +119,8 @@ class DelegationEventPactFolderProviderVerificationTest {
             resourceType = DelegationResourceType.ACCOUNT,
             resourceId = RESOURCE,
             capabilities = setOf(DelegationCapability.ACCOUNT_READ_BALANCES),
+            approvalPolicy = ApprovalPolicy.N_OF_M,
+            requiredApprovals = 3,
             validFrom = VALID_FROM,
             validTo = null,
             perTransactionLimit = EventMoney(BigDecimal("1500.00"), "CZK"),
